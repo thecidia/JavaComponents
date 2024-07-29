1d484aca873bfc91764c1e22c0099d2f975a5640
thecidia <thecid.iapv@gmail.com>
2024-07-28 21:40:33 -0600

    update

--- app/src/main/java/com/example/code/App.java
+++ app/src/main/java/com/example/code/App.java
@@ -3,6 +3,6 @@ package com.example.code;
 public class App {
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-		// TODO: is null
+		// XXX: this 'Method' is use for a test a code (Java-Components);
 	}
 }
